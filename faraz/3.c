0a776d2218c3e95f62fc8e38c4648aa187325574
Faraz Fallahi <fffaraz@gmail.com>
2014-05-15 12:42:10 +0430

    faraz

---
+++ faraz/3.c
@@ -0,0 +1,171 @@
+/*****************************************************
+This program was produced by the
+CodeWizardAVR V1.25.8 Professional
+Automatic Program Generator
+© Copyright 1998-2007 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Project : 
+Version : 
+Date    : 5/7/2014
+Author  : F4CG                            
+Company : F4CG                            
+Comments: 
+
+
+Chip type           : ATmega32
+Program type        : Application
+Clock frequency     : 16.000000 MHz
+Memory model        : Small
+External SRAM size  : 0
+Data Stack size     : 512
+*****************************************************/
+
+#include <mega32.h>
+#include <stdlib.h>
+
+// Alphanumeric LCD Module functions
+#asm
+   .equ __lcd_port=0x18 ;PORTB
+#endasm
+#include <lcd.h>
+
+#include <delay.h>
+
+#define ADC_VREF_TYPE 0x00
+
+unsigned int adc_value;
+float voltage;
+char str_voltage[16];
+
+// Read the AD conversion result
+unsigned int read_adc(unsigned char adc_input)
+{
+ADMUX=adc_input | (ADC_VREF_TYPE & 0xff);
+// Delay needed for the stabilization of the ADC input voltage
+delay_us(10);
+// Start the AD conversion
+ADCSRA|=0x40;
+// Wait for the AD conversion to complete
+while ((ADCSRA & 0x10)==0);
+ADCSRA|=0x10;
+return ADCW;
+}
+
+// Declare your global variables here
+
+void main(void)
+{
+// Declare your local variables here
+
+// Input/Output Ports initialization
+// Port A initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTA=0x00;
+DDRA=0x00;
+
+// Port B initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTB=0x00;
+DDRB=0x00;
+
+// Port C initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTC=0x00;
+DDRC=0x00;
+
+// Port D initialization
+// Func7=In Func6=In Func5=In Func4=In Func3=In Func2=In Func1=In Func0=In 
+// State7=T State6=T State5=T State4=T State3=T State2=T State1=T State0=T 
+PORTD=0x00;
+DDRD=0x00;
+
+// Timer/Counter 0 initialization
+// Clock source: System Clock
+// Clock value: Timer 0 Stopped
+// Mode: Normal top=FFh
+// OC0 output: Disconnected
+TCCR0=0x00;
+TCNT0=0x00;
+OCR0=0x00;
+
+// Timer/Counter 1 initialization
+// Clock source: System Clock
+// Clock value: Timer 1 Stopped
+// Mode: Normal top=FFFFh
+// OC1A output: Discon.
+// OC1B output: Discon.
+// Noise Canceler: Off
+// Input Capture on Falling Edge
+// Timer 1 Overflow Interrupt: Off
+// Input Capture Interrupt: Off
+// Compare A Match Interrupt: Off
+// Compare B Match Interrupt: Off
+TCCR1A=0x00;
+TCCR1B=0x00;
+TCNT1H=0x00;
+TCNT1L=0x00;
+ICR1H=0x00;
+ICR1L=0x00;
+OCR1AH=0x00;
+OCR1AL=0x00;
+OCR1BH=0x00;
+OCR1BL=0x00;
+
+// Timer/Counter 2 initialization
+// Clock source: System Clock
+// Clock value: Timer 2 Stopped
+// Mode: Normal top=FFh
+// OC2 output: Disconnected
+ASSR=0x00;
+TCCR2=0x00;
+TCNT2=0x00;
+OCR2=0x00;
+
+// External Interrupt(s) initialization
+// INT0: Off
+// INT1: Off
+// INT2: Off
+MCUCR=0x00;
+MCUCSR=0x00;
+
+// Timer(s)/Counter(s) Interrupt(s) initialization
+TIMSK=0x00;
+
+// Analog Comparator initialization
+// Analog Comparator: Off
+// Analog Comparator Input Capture by Timer/Counter 1: Off
+ACSR=0x80;
+SFIOR=0x00;
+
+// ADC initialization
+// ADC Clock frequency: 1000.000 kHz
+// ADC Voltage Reference: AREF pin
+ADMUX=ADC_VREF_TYPE & 0xff;
+ADCSRA=0x84;
+
+// LCD module initialization
+lcd_init(16);
+
+while (1)
+      {
+        delay_ms(200);  
+        lcd_clear();
+        
+        // Place your code here         
+        adc_value = read_adc(0);
+        voltage = (float)adc_value / 204.8;
+        ftoa(voltage,2,str_voltage);     
+        lcd_gotoxy(0,0);
+        lcd_puts(str_voltage);        
+        
+        adc_value = read_adc(1);
+        voltage = (float)adc_value / 204.8;
+        ftoa(voltage,2,str_voltage);  
+        lcd_gotoxy(0,1);
+        lcd_puts(str_voltage); 
+      };
+}
